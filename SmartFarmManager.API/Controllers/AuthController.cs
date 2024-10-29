16a52510db5f89ac82f558f3ade6262704a5fdb2
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-10-29 14:27:32 +0700

    update

---
+++ SmartFarmManager.API/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartFarmManager.Service.Interfaces;
+
+namespace SmartFarmManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        // Implement API endpoints for Authentication operations here
+    }
+}
