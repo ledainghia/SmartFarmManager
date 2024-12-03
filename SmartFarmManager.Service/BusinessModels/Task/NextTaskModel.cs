37746949db1a25e1ae03a9035af640dfc1683197
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2024-12-03 20:39:55 +0700

    update priorityNum for tasks

---
+++ SmartFarmManager.Service/BusinessModels/Task/NextTaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarmManager.Service.BusinessModels.Task
+{
+    public class NextTaskModel
+    {
+        public Guid TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string Cagename { get; set; }
+        public string AssignName { get; set; }
+        public int PriorityNum { get; set; }
+        public string Status { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int Total { get; set; }
+        public int TaskDone { get; set; }
+    }
+}
