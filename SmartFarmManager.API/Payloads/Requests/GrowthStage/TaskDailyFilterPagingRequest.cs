c6bd3453f8dbb5b58b3c84fe32074a64f27856c9
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-12-27 13:52:04 +0700

    [fea] add growstage api

---
+++ SmartFarmManager.API/Payloads/Requests/GrowthStage/TaskDailyFilterPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartFarmManager.API.Payloads.Requests.GrowthStage
+{
+    public class TaskDailyFilterPagingRequest
+    {
+        public string? TaskName { get; set; } // Lọc theo tên Task
+        public int? Session { get; set; } // Lọc theo buổi (Session)
+        public int? PageNumber { get; set; } = 1; // Số trang
+        public int? PageSize { get; set; } = 10; // Số phần tử mỗi trang
+    }
+}
