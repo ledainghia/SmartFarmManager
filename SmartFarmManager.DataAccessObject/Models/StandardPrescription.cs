06f597af5f3ba2e547bbc8a28ad3834af1d48caf
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-01-07 15:24:11 +0700

    update db

---
+++ SmartFarmManager.DataAccessObject/Models/StandardPrescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarmManager.DataAccessObject.Models
+{
+    public partial class StandardPrescription : EntityBase
+    {
+        public Guid DiseaseId { get; set; }
+
+        public string Notes { get; set; }
+
+        public virtual Disease Disease { get; set; }
+
+        public virtual ICollection<StandardPrescriptionMedication> StandardPrescriptionMedications { get; set; } = new List<StandardPrescriptionMedication>();
+    }
+}
