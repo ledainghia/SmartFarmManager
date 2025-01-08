3e4a5658535d3cc284dc8eebf4bfea5043947e92
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-01-08 10:37:29 +0700

    update create prescription

--- SmartFarmManager.Service/Shared/MedicalSymptomStatuseEnum.cs
+++ SmartFarmManager.Service/Shared/MedicalSymptomStatuseEnum.cs
@@ -8,8 +8,7 @@ namespace SmartFarmManager.Service.Shared
 {
     public class MedicalSymptomStatuseEnum
     {
-        public const string Normal = "Normal"; //Bình thường
-        public const string Diagnosed = "Diagnosed"; //Đã chuẩn đoán
+        public const string Rejected = "Rejected"; //Bình thường
         public const string Pending = "Pending"; //Đang chờ xem xét
         public const string Prescribed = "Prescribed"; //Đã kê thuốc
 
