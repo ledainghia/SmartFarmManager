732dd0e7cb687088a2c1df0ffdb0f549159e4f26
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-02-24 23:41:20 +0700

    create log vaccine

---
+++ SmartFarmManager.Service/BusinessModels/VaccineScheduleLog/CreateVaccineLogRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarmManager.Service.BusinessModels.VaccineScheduleLog
+{
+    public class CreateVaccineLogRequest
+    {
+        public DateTime Date { get; set; }
+        public int Session { get; set; }
+        public Guid VaccineId { get; set; }
+        public int Quantity { get; set; }
+        public string Notes { get; set; }
+        public string Photo { get; set; }
+        public Guid? TaskId { get; set; }
+    }
+}
