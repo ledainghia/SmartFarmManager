1a9c2ceb5d2769a7bba33de1637a236d7c3f1ed5
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-02-24 18:56:01 +0700

    create user in farm

--- SmartFarmManager.Service/BusinessModels/Users/UserCreateModel.cs
+++ SmartFarmManager.Service/BusinessModels/Users/UserCreateModel.cs
@@ -8,7 +8,6 @@ namespace SmartFarmManager.Service.BusinessModels.Users
 {
     public class UserCreateModel
     {
-        public string Username { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
