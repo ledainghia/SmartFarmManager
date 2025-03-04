eae45fcea711c11c53e96823e1c45df9da0bd738
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-03-04 22:04:21 +0700

    Update GetAllMedicalSymptomModel.cs

--- SmartFarmManager.Service/BusinessModels/MedicalSymptom/GetAllMedicalSymptomModel.cs
+++ SmartFarmManager.Service/BusinessModels/MedicalSymptom/GetAllMedicalSymptomModel.cs
@@ -13,7 +13,7 @@ namespace SmartFarmManager.Service.BusinessModels.MedicalSymptom
     {
         public Guid Id { get; set; }
         public Guid FarmingBatchId { get; set; }
-        public string Symtom { get; set; }
+        public string Symptoms { get; set; }
 
         public Guid? PrescriptionId { get; set; }
 
