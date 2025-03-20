73fc5e518022c596518ab1b8d7089fb262e7a384
Hau Nguyen <haunt150603@gmail.com>
2025-03-20 19:55:56 +0700

    [fea] apis for sensor data

---
+++ SmartFarmManager.Service/BusinessModels/SensorDataLog/SensorRecordModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarmManager.Service.BusinessModels.SensorDataLog
+{
+    public class SensorRecordModel
+    {
+        public DateTime BeginTime { get; set; }  // Thời gian bắt đầu
+        public DateTime EndTime { get; set; }    // Thời gian kết thúc
+        public double Value { get; set; }        // Giá trị sensor (giá trị đo được từ sensor)
+        public DateTime Date { get; set; }       // Ngày ghi nhận
+    }
+
+}
