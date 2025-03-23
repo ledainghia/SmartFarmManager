95856d69d8b5119a9ae4c3961a0f2cfb50895e0c
Hau Nguyen <haunt150603@gmail.com>
2025-03-23 14:37:32 +0700

    [fea] update api CRUD vaccine

---
+++ SmartFarmManager.API/Payloads/Requests/Vaccine/CreateVaccineRequest.cs
@@ -0,0 +1,37 @@
+﻿using SmartFarmManager.Service.BusinessModels.Vaccine;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartFarmManager.API.Payloads.Requests.Vaccine
+{
+    public class CreateVaccineRequest
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Method is required.")]
+        [StringLength(50, ErrorMessage = "Method cannot be longer than 50 characters.")]
+        public string Method { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "AgeStart is required.")]
+        public int AgeStart { get; set; }
+
+        [Required(ErrorMessage = "AgeEnd is required.")]
+        public int AgeEnd { get; set; }
+        public CreateVaccineModel MapToModel()
+        {
+            return new CreateVaccineModel
+            {
+                Name = Name,
+                Method = Method,
+                Price = Price,
+                AgeStart = AgeStart,
+                AgeEnd = AgeEnd
+            };
+        }
+    }
+}
