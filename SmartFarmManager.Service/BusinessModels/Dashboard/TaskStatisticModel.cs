f9417731e8ce7404cada5fbbc08b6859cb6aad49
Hau Nguyen <haunt150603@gmail.com>
2025-03-25 17:57:46 +0700

    Add FarmDashboard service, controller, and models
    Introduced IFarmDashboardService and its implementation FarmDashboardService to handle farm dashboard statistics. Updated ServiceExtensions.cs to include the new service in the dependency injection container. Created FarmDashboardController to manage API requests related to farm dashboard statistics.
    
    Added new business models in SmartFarmManager.Service.BusinessModels.Dashboard namespace: CageStatisticModel, DashboardStatisticsModel, FarmingBatchStatisticModel, StaffStatisticModel, TaskStatisticModel, and VaccineScheduleStatisticModel.
    
    Updated SmartFarmManager.Service.csproj to remove unnecessary item groups related to BusinessModels\NewFolder.

---
+++ SmartFarmManager.Service/BusinessModels/Dashboard/TaskStatisticModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarmManager.Service.BusinessModels.Dashboard
+{
+    public class TaskStatisticModel
+    {
+        public int TotalTasks { get; set; } // Tổng số công việc
+        public int PendingTasks { get; set; } // Công việc đang chờ
+        public int InProgressTasks { get; set; } // Công việc đang thực hiện
+        public int DoneTasks { get; set; } // Công việc đã hoàn thành
+        public int OverdueTasks { get; set; } // Công việc bị trễ hạn
+        public int CancelledTasks { get; set; } // Công việc bị hủy
+    }
+}
