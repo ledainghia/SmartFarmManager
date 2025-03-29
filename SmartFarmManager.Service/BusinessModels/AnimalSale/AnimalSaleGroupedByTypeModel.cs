e4c6d7dfb980a02e03a02882e617a5ccc69fd0a6
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-03-29 21:35:35 +0700

    get sale log by grow stage id

---
+++ SmartFarmManager.Service/BusinessModels/AnimalSale/AnimalSaleGroupedByTypeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFarmManager.Service.BusinessModels.AnimalSale
+{
+    public class AnimalSaleGroupedByTypeModel
+    {
+        public string SaleType { get; set; }
+        public int TotalQuantity { get; set; }
+        public double TotalRevenue { get; set; }
+        public double UnitPriceAverage { get; set; }
+        public List<AnimalSaleLogModel> Logs { get; set; }
+    }
+
+    public class AnimalSaleLogModel
+    {
+        public DateTime? SaleDate { get; set; }
+        public int Quantity { get; set; }
+        public double? UnitPrice { get; set; }
+        public double Total { get; set; }
+    }
+
+}
