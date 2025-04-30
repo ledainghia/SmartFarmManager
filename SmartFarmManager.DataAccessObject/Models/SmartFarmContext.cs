6ec2053478edd347a131a33db0617faa39f38103
Hau Nguyen <haunt150603@gmail.com>
2025-04-30 22:32:21 +0700

    UPDATE

--- SmartFarmManager.DataAccessObject/Models/SmartFarmContext.cs
+++ SmartFarmManager.DataAccessObject/Models/SmartFarmContext.cs
@@ -20,37 +20,37 @@ public partial class SmartFarmContext : DbContext
     {
     }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    if (!optionsBuilder.IsConfigured)
-    //    {
-    //        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-    //        IConfigurationRoot configuration = new ConfigurationBuilder()
-    //            .SetBasePath(Directory.GetCurrentDirectory())
-    //            .AddJsonFile("appsettings.json")
-    //            .Build();
-
-    //        string connectionString;
-
-    //        if (environment == "Development")
-    //        {
-    //            connectionString = configuration.GetConnectionString("DefaultConnection");
-    //        }
-    //        else // Production
-    //        {
-    //            connectionString = configuration.GetConnectionString("ProductConnection");
-    //        }
-
-    //        optionsBuilder.UseSqlServer(connectionString);
-    //    }
-    //}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //optionsBuilder.UseSqlServer("Server=localhost;Database=Farm3;User Id=sa;Password=12345;Encrypt=True;TrustServerCertificate=True;");
-        optionsBuilder.UseSqlServer("Server=103.48.193.165,5053;Database=FarmCapstoneProject;User Id=sa;Password=YourStronggg@Passw0rd;Encrypt=True;TrustServerCertificate=True;");
+        if (!optionsBuilder.IsConfigured)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString;
+
+            if (environment == "Development")
+            {
+                connectionString = configuration.GetConnectionString("DefaultConnection");
+            }
+            else // Production
+            {
+                connectionString = configuration.GetConnectionString("ProductConnection");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    //optionsBuilder.UseSqlServer("Server=localhost;Database=Farm3;User Id=sa;Password=12345;Encrypt=True;TrustServerCertificate=True;");
+    //    optionsBuilder.UseSqlServer("Server=103.48.193.165,5053;Database=FarmCapstoneProject;User Id=sa;Password=YourStronggg@Passw0rd;Encrypt=True;TrustServerCertificate=True;");
+
+    //}
 
     public virtual DbSet<AnimalSale> AnimalSales { get; set; }
 
